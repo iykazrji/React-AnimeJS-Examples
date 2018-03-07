17eae2d9d4ee7fd630514a65a8890528463d1a5a
iyk-azorji <iykazorji@gmail.com>
2018-03-07 15:22:29 +0100

    Refactored Timeline animations with callback to allow completion..

--- src/pages/timeline-animations/timeline-animations.js
+++ src/pages/timeline-animations/timeline-animations.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Anime from 'animejs';
 import Styled from 'styled-components';
-import { 
+import {
     BgContainer,
     PageTitleP1,
     AnimeButton,
     color_green
-} from '../../styled-components/styled-components'; 
+} from '../../styled-components/styled-components';
 import AnimationButton from '../../components/animation-button';
 
 let TimelineContainer = Styled.div`
@@ -36,10 +36,10 @@ let TimelineElement = Styled.div`
     }
 `
 
-let animation = null; 
+let animation = null;
 
 const clearCurrentAnimation = (currentAnimation) => {
-    if(currentAnimation){
+    if (currentAnimation) {
         console.log('Remove current animation...')
         currentAnimation.pause()
     }
@@ -47,88 +47,128 @@ const clearCurrentAnimation = (currentAnimation) => {
 
 const TimeLineAnimationStart = (target) => {
 
-    clearCurrentAnimation(animation)
+    return new Promise((resolve, reject) => {
+        clearCurrentAnimation(animation)
 
-    let target_container = target;
-    let target_parent = target.parentNode;
-    let target_childeren = target.childNodes;
-    animation = Anime.timeline();
-    animation
-    .add({ 
-        targets: target_container,
-        width: ['10%','80%'],
-        duration: 1500,
-        elasticity: 100
-    }).add({
-        targets: target_childeren,
-        translateY: ['-50px', '0px'],
-        opacity: [0, 1],
-        duration: 900,
-        elasticity: 300,
-        delay: (el, l) => {
-            return (l*100)
-        }
+        let target_container = target;
+        let target_parent = target.parentNode;
+        let target_childeren = target.childNodes;
+        let enter_complete = false;
+        animation = Anime.timeline();
+        animation
+            .add({
+                targets: target_container,
+                width: ['10%', '80%'],
+                duration: 1500,
+                elasticity: 100
+            }).add({
+                targets: target_childeren,
+                translateY: ['-50px', '0px'],
+                opacity: [0, 1],
+                duration: 900,
+                complete: () => {
+                    console.log("Enter animation complete...");
+                    enter_complete = true;
+                    resolve(enter_complete);
+                },
+                elasticity: 300,
+                delay: (el, l) => {
+                    return (l * 100)
+                }
+            })
     })
+
 }
 
 const TimeLineAnimationReturn = (target) => {
+    return new Promise((resolve, reject) => {
+        clearCurrentAnimation(animation)
 
-    clearCurrentAnimation(animation)
+        let target_container = target;
+        let target_parent = target.parentNode;
+        let target_childeren = target.childNodes;
+        let exit_complete = false;
+        animation = Anime.timeline();
+        animation
+            .add({
+                targets: target_childeren,
+                translateY: -50,
+                opacity: [1, 0],
+                duration: 800,
+                delay: (el, l) => {
+                    return (l * 100)
+                },
+                easing: 'easeOutSine',
 
-    let target_container = target;
-    let target_parent = target.parentNode;
-    let target_childeren = target.childNodes;
-
-    animation = Anime.timeline();
-    animation
-    .add({
-        targets: target_childeren,
-        translateY: -50,
-        opacity: [1, 0],
-        duration: 800,
-        delay: (el, l) =>{
-            return (l * 100)
-        },
-        easing: 'easeOutSine',
-
-    }).add({
-        targets: target_container,
-        width: ['80%', '10%'],
-        duration: 1600,
-        elasticity: 150,
+            }).add({
+                targets: target_container,
+                width: ['80%', '10%'],
+                duration: 1600,
+                elasticity: 150,
+                complete: () => {
+                    console.log("Exit animation complete...");
+                    exit_complete = true;
+                    resolve(exit_complete);
+                }
+            })
     })
 }
 
 class TimelineAnimations extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            animate: true
+            animate: true,
+            inProgress: false
         }
-        this.setOrReset = this.setOrReset.bind(this)
+        this.setOrReset = this.setOrReset.bind(this);
+
     }
-    setOrReset(){
-        this.setState({
-            animate: !this.state.animate
-        })
-        if(this.state.animate){
-            TimeLineAnimationStart(this.node)
-        }else{
-            TimeLineAnimationReturn(this.node)
+    setOrReset() {
+        console.log(this.state.inProgress)
+        if (!this.state.inProgress) {
+            if (this.state.animate) {
+                this.setState({
+                    inProgress: true
+                });
+                TimeLineAnimationStart(this.node).then(
+                    () => {
+                        console.log("Time to enter...")
+                        this.setState({
+                            animate: false,
+                            inProgress: false
+                        })
+                    }
+                )
+            } else {
+                this.setState({
+                    inProgress: true
+                })
+                TimeLineAnimationReturn(this.node).then(
+                    () => {
+                        console.log("Time to Exit")
+                        this.setState({
+                            animate: true,
+                            inProgress: false
+                        })
+                    }
+                )
+            }
         }
+
     }
     render() {
         return (
             <div>
                 <BgContainer className="bg-container">
                     <PageTitleP1>Timeline Animations</PageTitleP1>
-                    <TimelineContainer innerRef={ node => {this.node = node}}>
+                    <TimelineContainer innerRef={node => { this.node = node }}>
                         <TimelineElement className="timeline_element"><span>1</span></TimelineElement>
                         <TimelineElement className="timeline_element"><span>2</span></TimelineElement>
                         <TimelineElement className="timeline_element"><span>3</span></TimelineElement>
                     </TimelineContainer>
-                    <AnimationButton handleClick={this.setOrReset}/>
-                </BgContainer>          
+                    <AnimationButton handleClick={this.setOrReset} />
+                </BgContainer>
             </div>
         );
     }
