184f791cbf6067f8ae4af473d9dc15ddd235286b
iyk-azorji <iykazorji@gmail.com>
2018-03-08 03:28:26 +0100

    Updated text-animations to use componentDidUpdate to track for state changes

--- src/pages/text-animations/text-animations.js
+++ src/pages/text-animations/text-animations.js
@@ -7,6 +7,7 @@ import {
     AnimeButton,
     color_green
 } from '../../styled-components/styled-components';
+import AnimationButton from '../../components/animation-button';
 
 let TextAnimationContainer = Styled.div`
     overflow: hidden;
@@ -18,13 +19,17 @@ let TextAnimationContainer = Styled.div`
     margin-bottom: 200px;
 `
 let TextAnimationSpan = Styled.span`
-    font-size: 30px;
+    font-size: 35px;
     letter-spacing: 10px;
     font-weight: 600;
     text-transform: uppercase;
     text-align: center;
     margin: 0 auto;
     display: block;
+    .span-item{
+        display: inline-block;
+        transform: translateY(-110%);
+    }
 `
 let CharItem = Styled.span``
 
@@ -44,7 +49,7 @@ const SplitText = (text) => {
     return spanArray;
 }
 
-const SplitWord = (word) => {
+const SplitWord = (text) => {
     let count = -1;
     let wordArray = text.split(" ");
     let textArray = wordArray.map(word => {
@@ -59,16 +64,81 @@ const SplitWord = (word) => {
     console.log("Text array: " + textArray);
     return textArray;
 }
+let animation = null;
+
+const clearAnimation = (currentAnimation) => {
+    if (currentAnimation) {
+        currentAnimation.pause();
+    }
+}
+
+let animateTextIn = (target) =>{
+    clearAnimation(animation);
+    let spanItem = target.querySelectorAll(".span-item");
+    animation = Anime({
+        targets: spanItem,
+        opacity: [0, 1],
+        duration: 500,
+        easing: 'easeOutExpo',
+        translateY: ['-110%', '0%'],
+        delay: (el, l)=>{
+            return 150 + 40 * l
+        }
+    })
+}
+
+let animateTextOut = (target) => {
+    clearAnimation(animation);
+    let spanItem = target.querySelectorAll(".span-item");
+    animation = Anime({
+        targets: spanItem,
+        opacity: 0,
+        duration: 500,
+        easing: 'easeOutExpo',
+        translateY: ['-110%'],
+        delay: (el, l)=>{
+            return 200 + 80 * l
+        }
+    })
+}
+
 
 class TextAnimations extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            animate: false
+        }
+        this.setOrReset = this.setOrReset.bind(this)
+    }
+    setOrReset(){
+        this.setState({
+            animate: !this.state.animate
+        })
+    }
+
+    // Since there's no assurance of Synchronous State change update,
+    // We'd use a componentDidUpdate to track for changes in animation
+    // State...
+    componentDidUpdate(prevProps, prevState){
+        if(this.state.animate){
+            animateTextIn(this.animation_span)
+        }else{
+            animateTextOut(this.animation_span)
+        }
+    }
+    componentDidMount(){
+        animateTextIn(this.animation_span)
+    }
     render() {
         return (
             <div>
                 <BgContainer className="bg-container">
                     <PageTitleP1>Text Animations</PageTitleP1>
                     <TextAnimationContainer>
-                        <TextAnimationSpan>Free Fallin'</TextAnimationSpan>
+                        <TextAnimationSpan innerRef={ node => { this.animation_span = node }}>{ SplitWord("Free Fallin'") }</TextAnimationSpan>
                     </TextAnimationContainer>
+                    <AnimationButton handleClick={this.setOrReset} />
                 </BgContainer>
             </div>
         );
