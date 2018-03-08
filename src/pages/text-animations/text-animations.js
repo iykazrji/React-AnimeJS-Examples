d0b40b0ed60b8b97e1b9c187fedc639ae4231cf7
iyk-azorji <iykazorji@gmail.com>
2018-03-08 02:05:22 +0100

    Added split text logic

--- src/pages/text-animations/text-animations.js
+++ src/pages/text-animations/text-animations.js
@@ -14,16 +14,51 @@ let TextAnimationContainer = Styled.div`
     height: auto;
     margin: 0 auto;
     color: ${color_green};
-    margin-top: 50px;
-    margin-bottom: 50px;
+    margin-top: 200px;
+    margin-bottom: 200px;
 `
 let TextAnimationSpan = Styled.span`
     font-size: 30px;
-    letter-spacing: 1.5px;
+    letter-spacing: 10px;
     font-weight: 600;
     text-transform: uppercase;
     text-align: center;
+    margin: 0 auto;
+    display: block;
 `
+let CharItem = Styled.span``
+
+let WordItem = Styled.div`
+    display: inline-block;
+    margin-right: 20px;
+`
+
+const SplitText = (text) => {
+    let textArray = text.split("");
+    let count = -1;
+
+    let spanArray = textArray.map(value => {
+        count++;
+        return <span className="span-item" key={count}>{value !== " " ? value : "\u00A0"}</span>;
+    });
+    return spanArray;
+}
+
+const SplitWord = (word) => {
+    let count = -1;
+    let wordArray = text.split(" ");
+    let textArray = wordArray.map(word => {
+        count++;
+        return (
+            <WordItem key={count}>
+                {SplitText(word)}
+            </WordItem>
+        );
+    });
+
+    console.log("Text array: " + textArray);
+    return textArray;
+}
 
 class TextAnimations extends Component {
     render() {
